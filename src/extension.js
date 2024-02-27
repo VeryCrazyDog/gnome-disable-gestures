ef41b66de7c5065e3b8d03358110dbb17e076c36
VCD <VeryCrazyDog@gmail.com>
2024-02-27 23:34:32 +0800

    Update code based on extension review comments

--- src/extension.js
+++ src/extension.js
@@ -2,31 +2,36 @@
  * Disable Gestures 2021
  *
  * A GNOME extension that disables built-in gestures. Useful for kiosks and touch screen apps.
- * 
  */
-
-/* exported init */
-
 export default class Extension {
-  enable () {
-    global.stage.get_actions().forEach(a => { a.enabled = false })
+  focusWindowId = null
+  inFullscreenChangedId = null
+
+  enable() {
+    global.stage.get_actions().forEach(action => { action.enabled = false })
     const disableUnmaximizeGesture = () => {
-      global.stage.get_actions().forEach(a => { if (a !== this) { a.enabled = false } })
+      global.stage.get_actions().forEach(action => {
+        if (action === this) return
+        action.enabled = false
+      })
     }
-    global.display.connect('notify::focus-window', disableUnmaximizeGesture)
-    global.display.connect('in-fullscreen-changed', disableUnmaximizeGesture)
+    if (this.focusWindowId === null) {
+      this.focusWindowId = global.display.connect('notify::focus-window', disableUnmaximizeGesture)
+    }
+    if (this.inFullscreenChangedId === null) {
+      this.inFullscreenChangedId = global.display.connect('in-fullscreen-changed', disableUnmaximizeGesture)
+    }
   }
 
-  disable () {
-    global.stage.get_actions().forEach(a => { a.enabled = true })
-    const enableUnmaximizeGesture = () => {
-      global.stage.get_actions().forEach(a => { if (a !== this) { a.enabled = true } })
+  disable() {
+    if (this.inFullscreenChangedId !== null) {
+      global.display.disconnect(this.inFullscreenChangedId)
+      this.inFullscreenChangedId = null
     }
-    global.display.connect('notify::focus-window', enableUnmaximizeGesture)
-    global.display.connect('in-fullscreen-changed', enableUnmaximizeGesture)
+    if (this.focusWindowId !== null) {
+      global.display.disconnect(this.focusWindowId)
+      this.focusWindowId = null
+    }
+    global.stage.get_actions().forEach(action => { action.enabled = true })
   }
-}
-
-function init () {
-  return new Extension()
 }
